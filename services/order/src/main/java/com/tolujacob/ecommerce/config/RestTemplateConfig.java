9497c6223f547449687291b3a6541fed2a4318af
tolujacob05 <tolujacob05@gmail.com>
2024-08-25 05:55:31 +0100

    gateway server added

--- services/order/src/main/java/com/tolujacob/ecommerce/config/RestTemplateConfig.java
+++ services/order/src/main/java/com/tolujacob/ecommerce/config/RestTemplateConfig.java
@@ -2,12 +2,13 @@ package com.tolujacob.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplate {
+public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return  new RestTemplate();
     }
 }
